e74fa5b27419294c7afa95fe92f1268c235c77e6
Nirmal Kumar <84436580+Nirmal-Kumar97@users.noreply.github.com>
2022-09-23 15:26:44 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,10 +29,13 @@ export class AppComponent {
 
   ngOnInit() {
     this.loginForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [
+      email: new FormControl('', [
         Validators.required,
-        Validators.minLength(6),
+        Validators.email,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+      ]),
+      password: new FormControl('', [
+        Validators.required
       ]),
     });
   }
